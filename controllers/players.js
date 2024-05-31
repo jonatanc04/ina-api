4c0c43eb338c38d08dfc212cbe037a0f365cd4e8
jonatanc04 <jcorcolesg98@gmail.com>
2024-05-31 15:37:58 +0200

    changed routes to model controller

---
+++ controllers/players.js
@@ -0,0 +1,55 @@
+import { PlayerModel } from "../models/player.js";
+import { validatePlayer, validatePartialPlayer } from '../schemas/players.js'
+
+export class PlayerController {
+
+  static async getAll (req, res) {
+    const { position } = req.query
+    const players = await PlayerModel.getAll({ position })
+    res.json(players)
+  }
+
+  static async getByID (req, res) {
+    const { id } = req.params
+    const player = await PlayerModel.getByID({ id })
+    if (player) return res.json(player)
+    res.status(404).json({ message: 'Player not found' })
+  }
+
+  static async create (req, res) {
+    const result = validatePlayer(req.body)
+  
+    if (!result.success) {
+      return res.status(400).json({ error: JSON.parse(result.error.message) })
+    }
+  
+    const newPlayer =  await PlayerModel.create({ input: result.data })
+  
+    res.status(201).json(newPlayer)
+  }
+
+  static async delete (req, res) {
+    const { id } = req.params
+    
+    const result = await PlayerModel.delete({ id })
+  
+    if (!result) {
+      return res.status(404).json({ message: 'Player not found' })
+    }
+  
+    return res.json({ message: 'Player deleted' })
+  }
+
+  static async update (req, res) {
+    const result = validatePartialPlayer(req.body)
+  
+    if (!result.success) {
+      return res.status(400).json({ error: JSON.parse(result.error.message) })
+    }
+  
+    const { id } = req.params
+    const updatedPlayer = await PlayerModel.update({id, input: result.data})
+  
+    return res.json(updatedPlayer)
+  }
+}
--- routes/players.js
+++ routes/players.js
@@ -1,55 +1,14 @@
 import { Router } from "express";
-import { validatePlayer, validatePartialPlayer } from '../schemas/players.js'
-import { PlayerModel } from "../models/player.js";
+import { PlayerController } from "../controllers/players.js";
 
 export const playersRouter = Router()
 
-playersRouter.get('/', async (req, res) => {
-  const { position } = req.query
-  const players = await PlayerModel.getAll({ position })
-  res.json(players)
-})
+playersRouter.get('/', PlayerController.getAll)
 
-playersRouter.get('/:id', async (req, res) => {
-  const { id } = req.params
-  const player = await PlayerModel.getByID({ id })
-  if (player) return res.json(player)
-  res.status(404).json({ message: 'Player not found' })
-})
+playersRouter.get('/:id', PlayerController.getByID)
 
-playersRouter.post('/', async (req, res) => {
-  const result = validatePlayer(req.body)
+playersRouter.post('/', PlayerController.create)
 
-  if (!result.success) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
-  }
+playersRouter.delete('/:id', PlayerController.delete)
 
-  const newPlayer =  await PlayerModel.create({ input: result.data })
-
-  res.status(201).json(newPlayer)
-})
-
-playersRouter.delete('/:id', async (req, res) => {
-  const { id } = req.params
-  
-  const result = await PlayerModel.delete({ id })
-
-  if (!result) {
-    return res.status(404).json({ message: 'Player not found' })
-  }
-
-  return res.json({ message: 'Player deleted' })
-})
-
-playersRouter.patch('/:id', async (req, res) => {
-  const result = validatePartialPlayer(req.body)
-
-  if (!result.success) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
-  }
-
-  const { id } = req.params
-  const updatedPlayer = await PlayerModel.update({id, input: result.data})
-
-  return res.json(updatedPlayer)
-})
+playersRouter.patch('/:id', PlayerController.update)
