3f00ea92d067ba004f536d402a9a30dcb3381433
jonatanc04 <jcorcolesg98@gmail.com>
2024-05-31 13:37:55 +0200

    schema updated with new data

--- schemas/players.js
+++ schemas/players.js
@@ -8,9 +8,21 @@ const playerSchema = z.object({
   nickname: z.string({
     invalid_type_error: 'Player nickname must be a string',
   }),
+  element: z.string({
+    invalid_type_error: 'Player element must be a string',
+    required_error: 'Player element is required.'
+  }),
+  gender: z.string({
+    invalid_type_error: 'Player gender must be a string',
+    required_error: 'Player gender is required.'
+  }),
   position: z.string({
     invalid_type_error: 'Movie position must be a string',
     required_error: 'Player position is required.'
+  }),
+  year: z.string({
+    invalid_type_error: 'Player year must be a string',
+    required_error: 'Player year is required.'
   }),
   sprite: z.string().url({
     message: 'Poster must be a valid URL'
